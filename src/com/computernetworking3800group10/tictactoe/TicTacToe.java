41961eb93d64c8b1f46b39740335068ddc892cd3
kiennguyenGH <ktnguyen4647@gmail.com>
2024-04-11 00:41:57 -0700

    Prep TicTacToe variables

--- src/com/computernetworking3800group10/tictactoe/TicTacToe.java
+++ src/com/computernetworking3800group10/tictactoe/TicTacToe.java
@@ -1,4 +1,5 @@
 package com.computernetworking3800group10.tictactoe;
+import java.awt.Font;
 import java.awt.image.BufferedImage;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -30,18 +31,50 @@ public class TicTacToe implements Runnable{
 	private BufferedImage redCircle;
 	
 	private String[][] spaces = new String[3][3];
+	private boolean turn = false;
+	private boolean circle = true;
+	private boolean accepted = false;
+	private boolean unableToCommunicateWithOpponent = false;
+	private boolean won = false;
+	private boolean enemyWon = false;
 	
+	private int lengthOfSpace = 328;
+	private int errors = 0;
 	
- 	
+	private int firstSpot = -1;
+	private int secondSpot = -1;
+	
+	private Font font = new Font("Times New Roman", Font.PLAIN, 32);
+	private Font smallerFont = new Font("Times New Roman", Font.PLAIN, 20);
+	private Font largerFont = new Font("Times New Roman", Font.PLAIN, 50);
+	
+	private String waitingString = "Waiting for another player";
+	private String unableToCommunicateWithOpponentString = "Unable to communicate with opponent.";
+	private String wonString = "You win!";
+	private String enemyWonString = "You lose!";
+	
 	
 	public TicTacToe()
 	{
+		System.out.println("Enter the IP Address: ");
+		ip = input.nextLine();
+		System.out.println("Enter the port: ");
+		port = input.nextInt();
+		while (port < 1 || port > 65535) {
+			System.out.println("Invalid port. Input another port: ");
+			port = input.nextInt();
+		}
 		
+		loadImages();
 	}
 	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
+		
+	}
+	
+	private void loadImages() {
 		
 	}
 
